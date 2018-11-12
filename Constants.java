389bc388c0e254d78b9fcb64a6cd7a4ae5336588
rg31039 <44969608+rg31039@users.noreply.github.com>
2018-11-12 17:49:22 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ibm.marketing.approvalgroups.config;
+
+/**
+ * 
+ * @author Praveen Kumar Sukumaran
+ *
+ */
+public interface Constants {
+	
+	/**
+	 * Default API Approval Groups API Response
+	 * limit a.k.a page size
+	 */
+	String DEFAULT_API_RESPONSE_LIMIT = "25";
+	
+	/**
+	 * Default API Approval Groups API Response
+	 * offset a.k.a page number
+	 */
+	String DEFAULT_API_RESPONSE_OFFSET = "1";
+	
+	/**
+	 * Default API Approval Groups API Response
+	 * sorting order
+	 */
+	String DEFAULT_API_RESPONSE_SORT = "asc";
+
+}
